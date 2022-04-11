8f1e8268f7b8f33a6f9294aa405816113f22550d
Hanker <wu_2018@mail.ustc.edu.cn>
2022-04-11 14:51:20 +0000

    update generator

--- data/generator.py
+++ data/generator.py
@@ -1,104 +1,124 @@
 import random
 import json
+from turtle import color
 from PIL import Image
 from PIL import ImageDraw, ImageFont
 import os
-
+import numpy as np
+import colorsys
+from tqdm import tqdm
 
 class ImageChar:
     def __init__(self,
-                 height,
-                 num_char,
+                 backgroundDir='./bg_pics_small',
                  fontDir='./font'):
-
-        self.size = (height*num_char, height)
-        self.fontSize = int(height * 0.8)
+        
         self.fontDir = fontDir
-        self.num_char = num_char
+        self.backgroundDir = backgroundDir
+        self.image = None
+        self.pos = None
+        self.fontSize = None
 
-        self.bgColor = (255, 255, 255)
-        self.fontColor = (0, 0, 0)
-        self.fontPath = './font/simsun.ttc'
-        self.randomConfig()
+    def randomBackground(self):
+        bg_list = os.listdir(self.backgroundDir)
+        background = f'{self.backgroundDir}/{random.choice(bg_list)}'
+        
+        with Image.open(background) as im:
+            self.image = im.copy().convert('RGBA')
+            
+        self.fontSize = int(self.image.height * 0.15) 
 
-        self.font = ImageFont.truetype(self.fontPath, self.fontSize)
-        self.image = Image.new('RGB', self.size, self.bgColor)
+    def randomPosition(self, num_char, min_dist=True):
+        pos = []
+        height = self.image.height
+        width = self.image.width
+    
+        while True:
+            x = random.choice(
+                range(int(0.1*width), int(0.75*width))
+                )
+            y = random.choice(
+                range(int(0.1*height), int(0.75*height))
+                )
+            
+            add = True
+            if min_dist is not None:
+                for p in pos:
+                    if abs(p[0] - x) < min_dist and abs(p[1] - y) < min_dist:
+                        add = False
+                        break
+            if add:
+                pos.append((x, y))
+            if len(pos) >= num_char:
+                break
+        return pos
+        
 
-    def randomConfig(self):
-        self.bgColor = (random.randint(200, 255), random.randint(200, 255), random.randint(200, 255))
-        self.fontColor = (random.randint(0, 100), random.randint(0, 100), random.randint(0, 100))
-        font_name_list = os.listdir(self.fontDir)
-        fontPath = [os.path.join(self.fontDir, font_name) for font_name in font_name_list]
-        self.fontPath = random.choice(fontPath)
-
-    def rotate(self):
-        self.image.rotate(random.randint(0, 45), expand=False)
-
-    def drawText(self, pos, txt, fill):
-        draw = ImageDraw.Draw(self.image)
-        draw.text(pos, txt, font=self.font, fill=fill)
+    def drawChar(self, pos, rotate, char, font, color=None):
+        size = int(1.5 * self.fontSize)
+        txt=Image.new('RGBA', (size, size), (0, 0, 0, 0))
+        draw = ImageDraw.Draw(txt)
+        if color==None:
+            img_data = np.asarray(self.image)
+            bg_color = np.average(np.average(img_data[pos[1]:pos[1]+size, pos[0]:pos[0]+size], axis=0), axis=0)
+            r, g, b, _ = bg_color
+            h, _, _ = colorsys.rgb_to_hsv(r/255, g/255, b/255)
+            h = h+0.5 if h<0.5 else h-0.5
+            r, g, b = colorsys.hsv_to_rgb(h, 1, 1)
+            color = (int(r*255), int(g*255), int(b*255), 255)
+        elif len(color)==3:
+            color = (color[0], color[1], color[2], 255)
+        draw.text((0, 0), char, font=font, fill=color)
         del draw
+        w = txt.rotate(rotate, expand=True)
+        self.image.paste(w, pos, w)
+        return (pos[0], pos[1], w.height, w.width)
 
-    def randPoint(self):
-        (width, height) = self.size
-        return random.randint(0, width), random.randint(0, height)
-
-    def randLine(self, num):
-        draw = ImageDraw.Draw(self.image)
-        for i in range(0, num):
-            draw.line([self.randPoint(), self.randPoint()],
-                      (random.randint(128, 255), random.randint(128, 255), random.randint(128, 255)))
-        del draw
-
-    def randChinese(self, text, num_line):
-        gap = 8
+    def generate(self, text):
+        self.randomBackground()
         num = len(text)
-        start = 2
-        self.randLine(num_line//2)
+        pos = self.randomPosition(num, min_dist=1.5*self.fontSize)
+        rotate = [random.randint(-45, 45) for _ in range(num)]
+        font_name_list = os.listdir(self.fontDir)
+        fonts = [ImageFont.truetype(os.path.join(self.fontDir, f), self.fontSize) for f in font_name_list]
+        bbox = []
         for i in range(0, num):
-            x = start + self.fontSize * i + random.randint(2, gap) + gap * i
-            self.drawText((x, random.randint(0, 5)), text[i], self.fontColor)
-            self.randomConfig()
-            self.rotate()
-        self.randLine(num_line//2)
+            bbox.append(self.drawChar(pos[i], rotate[i], text[i], random.choice(fonts)))
+        return bbox
 
     def save(self, path):
         self.image.save(path, 'png')
 
-    def generate(self, text, dirty):
-        assert len(text) == self.num_char, "Number of char in the class doesn't much the text length"
-        self.randChinese(text, dirty)
 
-
-def generate(name='captcha_single', split='train', dirty=20):
-    data_dir = f"./image/{name}_{dirty:02}"
+def generate(name='captcha_single', split='train', num=1000):
+    data_dir = f"./image/{name}"
     split_dir = os.path.join(data_dir, split)
     if not os.path.exists(split_dir):
         os.makedirs(split_dir)
 
-    with open('./text/word_1ch.txt', 'r') as fp:
+    with open('./text/character_3000.txt', 'r') as fp:
         word_list = fp.read().split()
         word_dict = {k: v for v, k in enumerate(word_list)}  # {"/u2341": 3}
 
-    with open('./text/word_2ch.txt', 'r') as fp:
-        text = fp.read().split()  # ["出生", "中国", "背景", ...]
-        index_info = []
 
-        repeat_time = 10 if split == 'train' else 1
+    index_info = []
+    ic = ImageChar()
 
-        for t in range(repeat_time):
-            for i, word in enumerate(text):
-                data_id = t * len(text) + i
-                img_path = f"{split_dir}/{split}_{data_id:05d}.png"
-
-                index_info.append({'data_id': f'{data_id:05d}',
-                                   'label': word,
-                                   'label_id': [word_dict[word[0]], word_dict[word[1]]],
-                                   'image_path': f"{split}_{data_id:05d}.png"})
-
-                ic = ImageChar(height=40, num_char=2, fontDir='./font')
-                ic.generate(word, dirty)
-                ic.save(img_path)
+    print(f'| Generating {name} {split}')
+    for i in tqdm(range(num)):
+        data_id = i
+        img_path = f"{split_dir}/{split}_{data_id:05d}.png"
+        num_char = random.choice([4, 5])
+        word = ''
+        for _ in range(num_char):
+            word += random.choice(word_list)
+        bbox = ic.generate(word)
+        index_info.append({'data_id': f'{data_id:05d}',
+                            'word_label': word,
+                            'word_label_id': [word_dict[w] for w in word],
+                            'bbox': bbox,
+                            'image_path': f"{split}_{data_id:05d}.png"})
+        ic.save(img_path)
 
     with open(os.path.join(data_dir, f'{split}_info.json'), 'w') as info_file:
         json.dump(index_info, info_file, indent=4)
@@ -106,40 +126,8 @@ def generate(name='captcha_single', split='train', dirty=20):
 
 
 if __name__ == '__main__':
-    generate("captcha_2ch", "test", 0)
-    generate("captcha_2ch", "valid", 0)
-    generate("captcha_2ch", "train", 0)
+    generate("captcha_click", "test", 1000)
+    generate("captcha_click", "valid", 2000)
+    generate("captcha_click", "train", 30000)
 
-    generate("captcha_2ch", "test", 10)
-    generate("captcha_2ch", "valid", 10)
-    generate("captcha_2ch", "train", 10)
-
-    generate("captcha_2ch", "test", 20)
-    generate("captcha_2ch", "valid", 20)
-    generate("captcha_2ch", "train", 20)
-
-    generate("captcha_2ch", "test", 30)
-    generate("captcha_2ch", "valid", 30)
-    generate("captcha_2ch", "train", 30)
-
-    generate("captcha_2ch", "test", 40)
-    generate("captcha_2ch", "valid", 40)
-    generate("captcha_2ch", "train", 40)
-
-    generate("captcha_2ch", "test", 50)
-    generate("captcha_2ch", "valid", 50)
-    generate("captcha_2ch", "train", 50)
-
-    generate("captcha_2ch", "test", 60)
-    generate("captcha_2ch", "valid", 60)
-    generate("captcha_2ch", "train", 60)
-
-    generate("captcha_2ch", "test", 70)
-    generate("captcha_2ch", "valid", 70)
-    generate("captcha_2ch", "train", 70)
-
-    generate("captcha_2ch", "test", 80)
-    generate("captcha_2ch", "valid", 80)
-    generate("captcha_2ch", "train", 80)
-
-
+    
