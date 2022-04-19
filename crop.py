2c829e8a229a1298ea405b080028b3f3f8eaab0a
Hanker <wu_2018@mail.ustc.edu.cn>
2022-04-19 02:06:37 +0000

    add filter

--- crop.py
+++ crop.py
@@ -23,10 +23,10 @@ def line_length(pair):
 def harris_detect(img):
     dst = cv.cornerHarris(img, 2, 3, 0.02)
     new_img = np.zeros_like(img)
-    new_img[dst > 0.2*dst.max()] = 255
+    new_img[dst > 0.1*dst.max()] = 255
     return new_img, np.array(np.where(dst > 0.1*dst.max())).transpose()
 
-def diag_detect(img, th=1):
+def diag_detect(img, th=3):
     corner, points = harris_detect(img)
     dist_x = points[:, 0].reshape(-1, 1) - points[:, 0].reshape(1, -1)
     dist_y = points[:, 1].reshape(-1, 1) - points[:, 1].reshape(1, -1)
@@ -59,6 +59,11 @@ def merge_pairs(diag_point_pairs, min_dist=10):
             merged_pairs[len(merged_pairs)] = [pair]
     return [v[0] for k, v in merged_pairs.items()]
 
+def diag_to_square(diag_pair):
+    width = abs(diag_pair[0][0] - diag_pair[1][0])
+    height = abs(diag_pair[0][1] - diag_pair[1][1])
+    return (min(diag_pair[0][0], diag_pair[1][0]), min(diag_pair[0][1], diag_pair[1][1]), width, height)
+
 def clean_pairs(merged_pairs, img):
     lengths = [line_length(p) for p in merged_pairs]
     cleaned_pairs = []
@@ -66,14 +71,42 @@ def clean_pairs(merged_pairs, img):
         if length <= 40 or length >= 80:
             continue
         center = (int((pair[0][1]+pair[1][1])/2), int((pair[0][0]+pair[1][0])/2))
-        if img[center]:
-            cleaned_pairs.append(pair)
-    return cleaned_pairs
-
-def diag_to_square(diag_pair):
-    width = abs(diag_pair[0][0] - diag_pair[1][0])
-    height = abs(diag_pair[0][1] - diag_pair[1][1])
-    return (min(diag_pair[0][0], diag_pair[1][0]), min(diag_pair[0][1], diag_pair[1][1]), width, height)
+        if not img[center]:
+            continue
+        binary = cv.threshold(img, 127, 255, cv.THRESH_BINARY)[1]
+        rec = diag_to_square(pair)
+        area = rec[2]*rec[3]
+        rec_img = binary[rec[1]:rec[1]+rec[3], rec[0]:rec[0]+rec[2]]
+        if rec_img.sum() / rec_img.max() <= 0.8* area:
+            continue
+        cleaned_pairs.append(pair)
+    
+    for i in range(len(cleaned_pairs)):
+        if cleaned_pairs[i] is None:
+            continue
+        for j in range(i+1, len(cleaned_pairs)):
+            if cleaned_pairs[j] is None:
+                continue
+            reci = diag_to_square(cleaned_pairs[i])
+            recj = diag_to_square(cleaned_pairs[j])
+            
+            if reci[0] > recj[0] + recj[2] or\
+               reci[1] > recj[1] + recj[3] or\
+               reci[0] + reci[2] < recj[0] or\
+               reci[1] + reci[3] < recj[1]:
+                continue
+            
+            overlap_width = min(reci[0]+reci[2], recj[0]+recj[2]) - max(reci[0], recj[0])
+            overlap_height = min(reci[1]+reci[3], recj[1]+recj[3]) - max(reci[1], recj[1])
+            overlap = overlap_width*overlap_height
+            areai = reci[2]*reci[3]
+            areaj = recj[2]*recj[3]
+            if overlap / (areai + areaj - overlap) >= 0.4:
+                if areai < areaj:
+                    cleaned_pairs[i] = cleaned_pairs[j]
+                cleaned_pairs[j] = None
+    return [p for p in cleaned_pairs if p is not None]
+            
 
 def crop(img):
     diag_point_pairs = diag_detect(img)
